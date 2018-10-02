5360202e5e50df704e8830d24de1262afdab2849
lan74123 <lan74123@gmail.com>
2018-10-02 16:15:13 +0800

    add link to ngo

--- resources/views/frontend/trend/ngo.blade.php
+++ resources/views/frontend/trend/ngo.blade.php
@@ -29,7 +29,9 @@
                     </div>
                     <div class="service-lohas__box">
                         <div class="service-lohas__title">
-                            <h2>日本尊嚴死協會</h2>
+                            <a href="http://www.songenshi-kyokai.com/" target="_blank" rel="noopener noreferrer">
+                                <h2>日本尊嚴死協會</h2>
+                            </a>
                             <h6>(Japan Society for Dying With Dignity)</h6>
                             <h6>主張簽署預立醫療指示，保障善終權日本最大病人自主權NGO會員人數超過10萬</h6>
                         </div>
@@ -41,7 +43,9 @@
                     </div>
                     <div class="service-lohas__box">
                         <div class="service-lohas__title">
-                            <h2>千葉居家照護 市民網絡</h2>
+                            <a href="http://www.npo-pure.npo-jp.net/" target="_blank" rel="noopener noreferrer">
+                                <h2>千葉居家照護 市民網絡</h2>
+                            </a>
                             <h6>(NPO PURE)</h6>
                             <h6>以千葉大學福祉環境中心為基地全國性的居家癌症緩和醫療支援中心，尊重患者意願，以最少的痛苦提供居家安寧服務諮詢</h6>
                         </div>
@@ -59,7 +63,9 @@
                     </div>
                     <div class="service-lohas__box">
                         <div class="service-lohas__title">
-                            <h2>南韓國際照護權組織</h2>
+                            <a href="http://www.carerights.org/sub/about.php" target="_blank" rel="noopener noreferrer">
+                                <h2>南韓國際照護權組織</h2>
+                            </a>
                             <h6>(Care Rights International)</h6>
                             <h6>2012年創立於首爾推廣並捍衛年長病人的臨終自主權AD推廣及諮詢、聯合國NGO橫向合作</h6>
                         </div>
@@ -71,7 +77,9 @@
                     </div>
                     <div class="service-lohas__box">
                         <div class="service-lohas__title">
-                            <h2>南韓預立醫療指示立法推動會</h2>
+                            <a href="https://www.facebook.com/sasilmo/" target="_blank" rel="noopener noreferrer">
+                                <h2>南韓預立醫療指示立法推動會</h2>
+                            </a>
                             <h6>(Korean Initiative for Advance Directive)</h6>
                             <h6>致力於推動預立醫療指示之立法提供AD推廣及諮詢服務</h6>
                         </div>
@@ -89,7 +97,9 @@
                     </div>
                     <div class="service-lohas__box">
                         <div class="service-lohas__title">
-                            <h2>美善生命計畫</h2>
+                            <a href="http://enable.hku.hk/tch/main/main.aspx?str_section=journey" target="_blank" rel="noopener noreferrer">
+                                <h2>美善生命計畫</h2>
+                            </a>
                             <h6>(香港大學行為健康教研中心)</h6>
                             <h6>推廣大眾對於善終自主權之認識，推動生死教育的社會意識運動並以香港大學為中心推動各項相關研究計畫</h6>
                         </div>
@@ -107,7 +117,9 @@
                     </div>
                     <div class="service-lohas__box">
                         <div class="service-lohas__title">
-                            <h2>北京生前預囑推廣協會</h2>
+                            <a href="http://www.xzyzy.com/" target="_blank" rel="noopener noreferrer">
+                                <h2>北京生前預囑推廣協會</h2>
+                            </a>
                             <h6> </h6>
                             <h6>於2013年成立，前身為「選擇與尊嚴」公益網，中國大陸第一家推廣「尊嚴死」的NGO 致力於相關緩和醫療學科、機構和制度的建立與推廣</h6>
                         </div>
@@ -125,7 +137,9 @@
                     </div>
                     <div class="service-lohas__box">
                         <div class="service-lohas__title">
-                            <h2>新加坡慈懷理事會</h2>
+                            <a href="https://singaporehospice.org.sg/" target="_blank" rel="noopener noreferrer">
+                                <h2>新加坡慈懷理事會</h2>
+                            </a>
                             <h6>(Singapore Hospice Council)</h6>
                             <h6>新加坡各級醫院施行安寧照護之代表團體推廣安寧照護，並支持醫師、護士、照護者、志工的訓練新加坡於國際安寧界的發聲單位</h6>
                         </div>
@@ -137,7 +151,9 @@
                     </div>
                     <div class="service-lohas__box">
                         <div class="service-lohas__title">
-                            <h2>HCA Hospice Care</h2>
+                            <a href="https://www.hca.org.sg/" target="_blank" rel="noopener noreferrer">
+                                <h2>HCA Hospice Care</h2>
+                            </a>
                             <h6>(HCA)</h6>
                             <h6>成立於1989年，提供居家安寧照護之慈善團體，免費提供醫師、護士、社工、志工到家探訪之服務</h6>
                         </div>
@@ -155,7 +171,9 @@
                     </div>                     
                     <div class="service-lohas__box">
                         <div class="service-lohas__title">
-                            <h2>泰國安寧照護社群</h2>
+                            <a href="http://www.thaps.or.th/" target="_blank" rel="noopener noreferrer">
+                                <h2>泰國安寧照護社群</h2>
+                            </a>
                             <h6>(Thai Palliative Care Society : THAPS)</h6>
                             <h6>曾舉辦工作坊推廣預立醫療指示概念供安寧照護人士法律上之相關諮詢</h6>
                         </div>
@@ -167,7 +185,9 @@
                     </div>
                     <div class="service-lohas__box">
                         <div class="service-lohas__title">
-                            <h2>平靜善終</h2>
+                            <a href="http://happydeathday.co/" target="_blank" rel="noopener noreferrer">
+                                <h2>平靜善終</h2>
+                            </a>
                             <h6>(Happy Deathday)</h6>
                             <h6>曾辦過泰國最大規模之安寧照護推廣活動，集結泰國境內30餘個NGO共同策劃展覽</h6>
                         </div>
@@ -185,7 +205,9 @@
                         </div>
                         <div class="service-lohas__box">
                             <div class="service-lohas__title">
-                                <h2>Pallium India</h2>
+                                <a href="https://palliumindia.org/about/about-pallium-india/" target="_blank" rel="noopener noreferrer">
+                                    <h2>Pallium India</h2>
+                                </a>
                                 <h6> </h6>
                                 <h6>致力於推廣減緩病人痛苦及安寧照護在醫療體系的落實與印度中央政府有多項安寧照護與AD推廣的合作</h6>
                             </div>

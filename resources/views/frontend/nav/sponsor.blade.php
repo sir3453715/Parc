e882e41ec9eb28d05b3c5909bc73907bcb228c46
Kyle <KyleCW.Lin@ogilvy.com>
2019-12-16 15:44:06 +0800

    友善單位名稱跟連結顯示

--- resources/views/frontend/nav/sponsor.blade.php
+++ resources/views/frontend/nav/sponsor.blade.php
@@ -7,15 +7,26 @@
     <main class="container">
         <a href="#C" title="中央內容區塊" id="AC" accesskey="C" name="C">:::</a>
 
-        <!-- 合作單位 -->
+        <!-- 友善單位 -->
         <h2 class="title banner-sponsor__title">友善單位</h2>
         <!-- banner-sponsor -->
         <div class="row px-2 px-lg-0 banner-sponsor">
             @foreach($partner as $data)
             <div class="banner-sponsor__box">
-                <div class=" banner-sponsor__item">
-                    <img src="/storage/{{$data->pic}}" alt="{{$data->title}}" class="img-fluid" />
-                </div>
+                @isset($data->link)
+                    <a href="{{ $data->link }}" target="_blank">
+                        <div class=" banner-sponsor__item">
+                            <img src="/storage/{{$data->pic}}" alt="{{$data->title}}" class="img-fluid" />
+                        </div>
+                        <p align="center">{{$data->title}}</p>
+                    </a>
+                @endisset
+                @empty($data->link)
+                    <div class=" banner-sponsor__item">
+                        <img src="/storage/{{$data->pic}}" alt="{{$data->title}}" class="img-fluid" />
+                    </div>
+                    <p align="center">{{$data->title}}</p>
+                @endempty
             </div>
             @endforeach
         </div>
